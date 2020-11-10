7221c198247334f0fcef6a0ed5019503e87745cb
Klementina424 <58881767+Klementina424@users.noreply.github.com>
2020-11-11 00:53:55 +0300

    Update lab1_test.cpp

--- lab1_test.cpp
+++ lab1_test.cpp
@@ -1,6 +1,6 @@
-﻿//#include "pch.h"
+//#include "pch.h"
 #include "CppUnitTest.h"
-#include "/Users/varka/Documents/Алгоритмы/кхм/dualList.h"
+#include "dualList.h" /*/Users/varka/Documents/Алгоритмы/кхм/*/
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,24 +9,24 @@ namespace lab1test
 	TEST_CLASS(lab1test)
 	{
 	public:
-		dualList* test;
-		dualList* test_2;
+		dualList* current_dual_list;
+		dualList* another_dual_list;
 		TEST_METHOD_INITIALIZE(setup)
 		{
-			test = new dualList();
-			test_2 = new dualList();
+			current_dual_list = new dualList();
+			another_dual_list = new dualList();
 		}
 
 		TEST_METHOD(at_correct_index)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			Assert::AreEqual(test->at(1),2);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			Assert::AreEqual(current_dual_list->at(1),2);
 		}
 		TEST_METHOD(at_incorrect_index)
 		{
 			try {
-				test->at(-1);
+				current_dual_list->at(-1);
 			}
 			catch (std::out_of_range e) {
 				Assert::AreEqual("Index is greater than list size", e.what());
@@ -36,67 +36,67 @@ namespace lab1test
 
 		TEST_METHOD(test_IsEmptyTrue)
 		{
-			Assert::AreEqual(test->isEmpty(), true);
+			Assert::AreEqual(current_dual_list->isEmpty(), true);
 		}
 
 		TEST_METHOD(test_IsEmptyFalse)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			Assert::AreEqual(test->isEmpty(), false);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			Assert::AreEqual(current_dual_list->isEmpty(), false);
 		}
 
 		TEST_METHOD(test_PushFront)
 		{
-			test->push_front(3);
-			test->push_front(2);
-			test->push_front(1);
-			Assert::AreEqual(test->at(0), 1);
+			current_dual_list->push_front(3);
+			current_dual_list->push_front(2);
+			current_dual_list->push_front(1);
+			Assert::AreEqual(current_dual_list->at(0), 1);
 		}
 		TEST_METHOD(test_PushBack)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			Assert::AreEqual(test->at(2), 3);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			Assert::AreEqual(current_dual_list->at(2), 3);
 		}
 		TEST_METHOD(test_PushBackAndFront)
 		{
-			test->push_back(2);
-			test->push_back(3);
-			test->push_front(1);
-			test->push_back(4);
-			Assert::AreEqual(test->at(2), 3);
-			Assert::AreEqual(test->at(0), 1);
-			Assert::AreEqual(test->at(3), 4);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->push_front(1);
+			current_dual_list->push_back(4);
+			Assert::AreEqual(current_dual_list->at(2), 3);
+			Assert::AreEqual(current_dual_list->at(0), 1);
+			Assert::AreEqual(current_dual_list->at(3), 4);
 		}
 		TEST_METHOD(test_GetSze)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			Assert::AreEqual(test->get_size(), (size_t)3);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)3);
 		}
 		TEST_METHOD(test_GetSzeEmpty)
 		{
-			Assert::AreEqual(test->get_size(), (size_t)0);
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)0);
 		}
 
 		TEST_METHOD(test_PopBack)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->pop_back();
-			test->pop_back();
-			Assert::AreEqual(test->get_size(), (size_t)1);
-			Assert::AreEqual(test->at(0), 1);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->pop_back();
+			current_dual_list->pop_back();
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)1);
+			Assert::AreEqual(current_dual_list->at(0), 1);
 		}
 
 		TEST_METHOD(test_PopBackFromEmpty)
 		{
 			try {
-				test->pop_back();
+				current_dual_list->pop_back();
 			}
 			catch (std::out_of_range e) {
 				Assert::AreEqual("Segmentation fault", e.what());
@@ -104,19 +104,19 @@ namespace lab1test
 		}
 		TEST_METHOD(test_PopFront)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->pop_front();
-			test->pop_front();
-			Assert::AreEqual(test->get_size(), (size_t)1);
-			Assert::AreEqual(test->at(0), 3);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->pop_front();
+			current_dual_list->pop_front();
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)1);
+			Assert::AreEqual(current_dual_list->at(0), 3);
 		}
 
 		TEST_METHOD(test_PopFrontFromEmpty)
 		{
 			try {
-				test->pop_front();
+				current_dual_list->pop_front();
 			}
 			catch (std::out_of_range e) {
 				Assert::AreEqual("Segmentation fault", e.what());
@@ -125,58 +125,58 @@ namespace lab1test
 
 		TEST_METHOD(test_PopBackAndFrontFirst)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->push_front(0);
-			test->pop_front();
-			test->pop_back();
-			Assert::AreEqual(test->get_size(), (size_t)2);
-			Assert::AreEqual(test->at(0), 1);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->push_front(0);
+			current_dual_list->pop_front();
+			current_dual_list->pop_back();
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)2);
+			Assert::AreEqual(current_dual_list->at(0), 1);
 		}
 		TEST_METHOD(test_PopBackAndFrontLast)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->push_front(0);
-			test->pop_front();
-			test->pop_back();
-			Assert::AreEqual(test->get_size(), (size_t)2);
-			Assert::AreEqual(test->at(1), 2);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->push_front(0);
+			current_dual_list->pop_front();
+			current_dual_list->pop_back();
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)2);
+			Assert::AreEqual(current_dual_list->at(1), 2);
 		}
 
 		TEST_METHOD(test_Remove)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->remove(1);
-			Assert::AreEqual(test->get_size(), (size_t)2);
-			Assert::AreEqual(test->at(1), 3);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->remove(1);
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)2);
+			Assert::AreEqual(current_dual_list->at(1), 3);
 		}
 		TEST_METHOD(test_RemoveFirst)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->remove(0);
-			Assert::AreEqual(test->get_size(), (size_t)2);
-			Assert::AreEqual(test->at(0), 2);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->remove(0);
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)2);
+			Assert::AreEqual(current_dual_list->at(0), 2);
 		}
 		TEST_METHOD(test_RemoveLast)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->remove(2);
-			Assert::AreEqual(test->get_size(), (size_t)2);
-			Assert::AreEqual(test->at(1), 2);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->remove(2);
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)2);
+			Assert::AreEqual(current_dual_list->at(1), 2);
 		}
 		TEST_METHOD(test_Remove_incorrect_index)
 		{
 			try {
-				test->remove(-1);
+				current_dual_list->remove(-1);
 			}
 			catch (std::out_of_range e) {
 				Assert::AreEqual("Index is greater than list size", e.what());
@@ -185,30 +185,30 @@ namespace lab1test
 
 		TEST_METHOD(test_Insert)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(4);
-			test->insert(3, 2);
-			Assert::AreEqual(test->get_size(), (size_t)4);
-			Assert::AreEqual(test->at(2), 3);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(4);
+			current_dual_list->insert(3, 2);
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)4);
+			Assert::AreEqual(current_dual_list->at(2), 3);
 		}
 		TEST_METHOD(test_InsertFirst)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(4);
-			test->insert(0, 0);
-			Assert::AreEqual(test->get_size(), (size_t)4);
-			Assert::AreEqual(test->at(0), 0);
-			Assert::AreEqual(test->at(1), 1);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(4);
+			current_dual_list->insert(0, 0);
+			Assert::AreEqual(current_dual_list->get_size(), (size_t)4);
+			Assert::AreEqual(current_dual_list->at(0), 0);
+			Assert::AreEqual(current_dual_list->at(1), 1);
 		}
 		TEST_METHOD(test_Insert_incorrect_index)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
 			try {
-				test->insert(4, 3);
+				current_dual_list->insert(4, 3);
 			}
 			catch (std::out_of_range e) {
 				Assert::AreEqual("Index is greater than list size", e.what());
@@ -217,33 +217,33 @@ namespace lab1test
 
 		TEST_METHOD(test_Clear)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->clear();
-			Assert::AreEqual(test->isEmpty(), true);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->clear();
+			Assert::AreEqual(current_dual_list->isEmpty(), true);
 		}
 		TEST_METHOD(test_ClearEmpty)
 		{
-			test->clear();
-			Assert::AreEqual(test->isEmpty(), true);
+			current_dual_list->clear();
+			Assert::AreEqual(current_dual_list->isEmpty(), true);
 		}
 		TEST_METHOD(test_ClearBeforePush)
 		{
-			test->push_back(1);
-			test->clear();
-			test->push_back(1);
-			Assert::AreEqual(test->isEmpty(), false);
-			Assert::AreEqual(test->at(0), 1);
+			current_dual_list->push_back(1);
+			current_dual_list->clear();
+			current_dual_list->push_back(1);
+			Assert::AreEqual(current_dual_list->isEmpty(), false);
+			Assert::AreEqual(current_dual_list->at(0), 1);
 		}
 		TEST_METHOD(test_DoSTHAfterClear)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->clear();
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->clear();
 			try {
-				test->at(0);
+				current_dual_list->at(0);
 			}
 			catch (std::out_of_range e) {
 				Assert::AreEqual("Index is greater than list size", e.what());
@@ -252,86 +252,57 @@ namespace lab1test
 
 		TEST_METHOD(test_Set)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->set(1, 0);
-			Assert::AreEqual(test->at(1), 0);
-			Assert::AreEqual(test->at(2), 3);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->set(1, 0);
+			Assert::AreEqual(current_dual_list->at(1), 0);
+			Assert::AreEqual(current_dual_list->at(2), 3);
 		}
 
 		TEST_METHOD(test_SetAfterAndBefore)
 		{
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(3);
-			test->set(1, 0);
-			Assert::AreEqual(test->at(0), 1);
-			Assert::AreEqual(test->at(2), 3);
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->set(1, 0);
+			Assert::AreEqual(current_dual_list->at(0), 1);
+			Assert::AreEqual(current_dual_list->at(2), 3);
 		}
 		TEST_METHOD(test_Set_incorrect_index)
 		{
-			test->push_back(1);
+			current_dual_list->push_back(1);
 			try {
-				test->set(8, 0);
+				current_dual_list->set(8, 0);
 			}
 			catch (std::out_of_range e) {
 				Assert::AreEqual("Index is greater than list size", e.what());
 			}
 		}
-
-		TEST_METHOD(test_PushBackList)
+		TEST_METHOD(test_list_in_list)
 		{
-			test_2->push_back(3);
-			test_2->push_back(4);
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(*test_2);
-			Assert::AreEqual(test->get_size(), (size_t)4);
-			Assert::AreEqual(test->at(2), 3);
+			current_dual_list = new dualList();
+			another_dual_list = new dualList();
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->push_back(4);
+			another_dual_list->push_back(2);
+			another_dual_list->push_back(4);
+			Assert::AreEqual(current_dual_list->list_in_list(current_dual_list, another_dual_list), false);
 		}
-		TEST_METHOD(test_PushBackListLastItem)
+		TEST_METHOD(test_list_in_list2)
 		{
-			test_2->push_back(3);
-			test_2->push_back(4);
-			test->push_back(1);
-			test->push_back(2);
-			test->push_back(*test_2);
-			Assert::AreEqual(test->get_size(), (size_t)4);
-			Assert::AreEqual(test->at(3), 4);
+			current_dual_list = new dualList();
+			another_dual_list = new dualList();
+			current_dual_list->push_back(1);
+			current_dual_list->push_back(2);
+			current_dual_list->push_back(3);
+			current_dual_list->push_back(4);
+			another_dual_list->push_back(1);
+			another_dual_list->push_back(2);
+			Assert::AreEqual(current_dual_list->list_in_list(current_dual_list, another_dual_list), true);
 		}
-		TEST_METHOD(test_PushBackListFirstItem)
-		{
-			test_2->push_back(3);
-			test_2->push_back(4);
-			test->push_back(2);
-			test->push_front(1);
-			test->push_back(*test_2);
-			Assert::AreEqual(test->get_size(), (size_t)4);
-			Assert::AreEqual(test->at(0), 1);
-		}
-		TEST_METHOD(test_PushBackListWithChangeFront)
-		{
-			test_2->push_back(3);
-			test_2->push_back(4);
-			test->push_back(2);
-			test->push_front(1);
-			test->push_back(*test_2);
-			test->push_front(0);
-			Assert::AreEqual(test->get_size(), (size_t)5);
-			Assert::AreEqual(test->at(0), 0);
-		}
-		TEST_METHOD(test_PushBackListWithChangeLast)
-		{
-			test_2->push_back(3);
-			test_2->push_back(4);
-			test->push_back(2);
-			test->push_front(1);
-			test->push_back(*test_2);
-			test->push_front(0);
-			test->push_back(5);
-			Assert::AreEqual(test->get_size(), (size_t)6);
-			Assert::AreEqual(test->at(5), 5);
-		}
+		
 	};
 }
