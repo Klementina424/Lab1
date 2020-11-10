19012d1d53b99ea588697c4dfd5aed5f9fa7b7a4
Klementina424 <58881767+Klementina424@users.noreply.github.com>
2020-11-11 01:18:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,29 @@
-﻿#include <iostream>
+#include <iostream>
 #include "dualList.h"
 
 using namespace std;
 
 int main()
 {
-    dualList* testDualList=new dualList;
+    dualList* DualList=new dualList;
     size_t i, count;
-    int con;
+    int information;
     cout << "How many elements you want to add? ";
     cin >> count;
     for (i = 0; i < count; i++)
     {
         cout << i << " item is: ";
-        cin >> con;
-        testDualList->push_back(con);
+        cin >> information;
+        DualList->push_back(information);
     }
     cout << "ListNow: " << endl;
-    testDualList->print_to_console();
+    DualList->print_to_console();
     cout << endl;
-    cout <<"ListSize: " << testDualList->get_size() << endl;
+    cout <<"ListSize: " << DualList->get_size() << endl;
     cout << "Index item to insert and contend " << endl;
-    cin >> i >> con;
-    testDualList->insert(con, i);
+    cin >> i >> information;
+    DualList->insert(information, i);
     cout << "ListNow: "<< endl;
-    testDualList->print_to_console();
+    DualList->print_to_console();
 }
 
