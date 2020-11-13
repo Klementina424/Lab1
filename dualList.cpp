4574627faf561d0f43e6873210b164c9d2b190a2
Klementina424 <58881767+Klementina424@users.noreply.github.com>
2020-11-13 14:14:50 +0300

    Update dualList.cpp

--- dualList.cpp
+++ dualList.cpp
@@ -1,7 +1,7 @@
 #include "dualList.h"
 using namespace std;
 
-bool dualList::list_in_list(dualList* list_where)
+bool dualList::list_in_list(dualList*)
 {
 	Node* what_current = list_what->head;
 	for (Node* where_current = list_where->head; where_current != list_where->tail; where_current = where_current->next)
